f0134822e55be82685426b3ee19afa8fdec0551b
Frynelli <90149135+Frynelli@users.noreply.github.com>
2021-12-14 13:02:31 +0200

    js responsive icon

--- java.js
+++ java.js
@@ -1,7 +1,7 @@
 function getDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hourse < 10) {
+  if (hours < 10) {
     hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
@@ -29,15 +29,22 @@ function getTemperature(response) {
   let wind = document.querySelector("#wind");
   let temp = document.querySelector("#temperature");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+  let icon = response.data.weather[0].icon;
   dateElement.innerHTML = getDate(response.data.dt * 1000);
   temp.innerHTML = Math.round(response.data.main.temp);
   humidityElement.innerHTML = response.data.main.humidity;
   descriptionElement.innerHTML = response.data.weather[0].description;
   cityName.innerHTML = response.data.name;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  iconElement.setAttribute(
+    `src`,
+    `http://openweathermap.org/img/w/${icon}.png`
+  );
+  iconElement.setAttribute(`alt`, response.data.weather[0].description);
 }
 
 let apiKey = "16ee8d6616116203cc7a912f8467b2d5";
-let city = "London";
+let city = "Athens";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(getTemperature);
